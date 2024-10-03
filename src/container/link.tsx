f372094247159cc1d84f14b67ce943c2e48a396f
Vien.BIT <Vien.vu@bitelle.com>
2024-10-03 10:05:47 +0700

    12

--- src/container/link.tsx
+++ src/container/link.tsx
@@ -3,12 +3,12 @@ export default function Links() {
     <div className="about">
       <div className="about__content">
         <h1>Hi there</h1>
-        <h3>I'm Viên Vũ</h3>
+        <h3>I{"'"}m Viên Vũ</h3>
 
         <h3>Fullstack develper</h3>
 
         <p>
-          I'm a fullstack developer with 10 years of experience. I have worked
+          I{"'"}m a fullstack developer with 10 years of experience. I have worked
           with many technologies, such as React, Vue, Angular, Node, Express,
           NestJS, Laravel, and so on.
         </p>

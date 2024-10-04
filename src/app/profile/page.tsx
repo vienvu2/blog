e3fa4aea9af19d46408eecadd3cb8f11ad58ea60
Vien.BIT <Vien.vu@bitelle.com>
2024-10-04 14:49:30 +0700

    12

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,12 @@
+import About from "@/container/about";
+import Header from "@/container/header";
+
 export default function Home() {
   return (
     <div className="main">
-      <h1>Profile</h1>
+      <Header pathname="/" />
+      <About />
+      {/* <h1>Profile</h1> */}
     </div>
   );
 }
